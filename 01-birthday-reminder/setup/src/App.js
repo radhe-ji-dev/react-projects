a56adf33c4a568d8820a3cbd83d23d418f741253
Radhakant Tiwari <therktiwadi@gmail.com>
2021-12-26 16:47:39 +0530

    started

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <section className="container">
+        <h3>{people.length} Birthday Today</h3>
+
+        <List people={people} />
+
+        <button className="btn" onClick={() => setPeople([])}>
+          clear all
+        </button>
+      </section>
+    </>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
-const List = () => {
+import React from "react";
+import data from "./data";
+const List = ({ people }) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name}></img>
+            <div>
+              <h2>{name}</h2>
+              <h4>{age} years</h4>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
