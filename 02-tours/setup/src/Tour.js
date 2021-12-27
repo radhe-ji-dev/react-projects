6299d3bd0da69dbbda1b943586d4bb99fddfabf3
Radhakant Tiwari <therktiwadi@gmail.com>
2021-12-27 11:59:19 +0530

    reviews completed

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name}></img>
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 class="tour-price">${price}</h4>
+        </div>
+        <p>{readMore}info:`${info.substring(0,200)...`</p>
+        <p>{info}</p>
+        <button className="delete-btn">Not interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
